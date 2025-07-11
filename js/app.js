e04277335c70b7f5740266e5741fe23cbc26c711
Shariq Shakeel <shariqshakeel107@gmail.com>
2025-07-11 22:56:36 +0500

    initial

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+let names = ["Shariq","Shakeel","Javed","javeria","kashif","Usman"];
+
+for(i=0;i<names.length;i++){
+   
+   if(names[i][0] == "J" || names[i][0]=="j"){
+    console.log(`Goodbye ${names[i]}`)
+   }
+   else{
+console.log(`Hello ${names[i]}`)
+   }
+
+}
+
